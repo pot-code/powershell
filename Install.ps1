2ab32fb0e40318f8e801d475e2079f864c3aac49
pot-code <136290354@qq.com>
2025-06-03 08:36:09 +0800

    feat: 安装前判断是否已安装

--- Install.ps1
+++ Install.ps1
@@ -1,22 +1,26 @@
 # 1. 安装依赖
-winget install ajeetdsouza.zoxide
-Install-Module -Name PSFzf -RequiredVersion 2.6.7
+if (-not (Get-Command zoxide -ErrorAction SilentlyContinue)) {
+  winget install ajeetdsouza.zoxide
+}
+
+if (-not (Get-Module -ListAvailable -Name PSFzf)) {
+  Install-Module -Name PSFzf -RequiredVersion 2.6.7
+}
 
 # 2. 注册模块地址
 $modulePath = "$PSScriptRoot\Modules"
 [Environment]::SetEnvironmentVariable(
   'PSModulePath',
   "$($env:PSModulePath);$modulePath",
-  [System.EnvironmentVariableTarget]::Machine
+  [System.EnvironmentVariableTarget]::User
 )
 
 # 3. 链接 profile
 $target = $PROFILE
 
 if (Test-Path $target) {
-  Copy-Item $target $target.bak -Force
   Remove-Item $target
 }
 
-New-Item -Path $PROFILE -ItemType SymbolicLink -Target $PSScriptRoot\Profile.ps1
+New-Item -Path $target -ItemType SymbolicLink -Target $PSScriptRoot\Profile.ps1
 Write-Output "配置已链接到 $PROFILE，请重新打开 PowerShell"
